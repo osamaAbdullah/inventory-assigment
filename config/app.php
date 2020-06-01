aa1c9fdc75caf644043d3564801c9bae645d6a24
osama <osama@aso.com>
2020-06-01 22:57:36 +0300

    updated config

--- config/app.php
+++ config/app.php
@@ -5,18 +5,7 @@ return [
 	// the root url of the application like 'http://inventory-assignment.demo'
 	'root-url' => 'http://inventory-assignment.demo',
 	
-	'debugging' => true,
-	
-	'mail' => [
-		'MAIL_DRIVER' => 'smtp',
-		'MAIL_HOST' => 'smtp.mailtrap.io',
-		'MAIL_PORT' => '2525',
-		'MAIL_USERNAME' => '75f659ee1eacba',
-		'MAIL_PASSWORD' => '47c54354e9d527',
-		'MAIL_ENCRYPTION' => 'tls',
-	],
-	
-	// measured with minutes minutes
+	// measured with minutes
 	'password_reset_token_expiration' => 30,
 	
 	
